1240d8fa95ff1b94dd4c678c48c2fc39ab791e61
bryanlin16899 <bryanlin16899@outlook.com>
2024-02-14 15:41:00 +0800

    test cicd

--- src/app/api/postUpdate/route.ts
+++ src/app/api/postUpdate/route.ts
@@ -1,19 +1,17 @@
-import { exec } from "child_process";
-import { NextResponse } from "next/server";
-import path from "path";
 
-export async function GET(req: any, res: any) {
-    const blogVaultPath = path.join(process.cwd(), "public", "BlogVault");
+// deprecated
+// export async function GET(req: any, res: any) {
+//     const blogVaultPath = path.join(process.cwd(), "public", "BlogVault");
 
-    const result = await new Promise<string>((resolve, reject) => {
-        exec("docker exec my_blog git -C public/BlogVault pull", { cwd: blogVaultPath }, (error, stdout, stderr) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(stdout);
-            }
-        });
-    });
+//     const result = await new Promise<string>((resolve, reject) => {
+//         exec("docker exec my_blog git -C public/BlogVault pull", { cwd: blogVaultPath }, (error, stdout, stderr) => {
+//             if (error) {
+//                 reject(error);
+//             } else {
+//                 resolve(stdout);
+//             }
+//         });
+//     });
 
-    return NextResponse.json({ success: true, stdout: result });
-};
+//     return NextResponse.json({ success: true, stdout: result });
+// };
